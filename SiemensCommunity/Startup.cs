31131b0cc63608be6cddab1802d2cd16cd55dc8b
popahoreaserban <horea.popa.ext@siemens.com>
2021-06-14 14:03:21 +0300

    MVC support added

--- SiemensCommunity/Startup.cs
+++ SiemensCommunity/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +17,7 @@ namespace SiemensCommunity
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Latest);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +29,8 @@ namespace SiemensCommunity
             }
 
             app.UseRouting();
+
+            app.UseMvc();
 
             app.UseEndpoints(endpoints =>
             {
