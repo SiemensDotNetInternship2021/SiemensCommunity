a0ec360aacf675793b54a32198903f22971145e6
popahoreaserban <horea.popa.ext@siemens.com>
2021-06-27 16:59:38 +0300

    Added Angular

---
+++ SiemensCommunity-SPA/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'SiemensCommunity-SPA';
+  products: any;
+
+  constructor(private http: HttpClient)
+  {
+
+  }
+
+  ngOnInit() 
+  {
+    this.http.get("http://localhost:52718/api/Product/Get").subscribe(response => {
+      this.products = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
